bbf544bf47edb89a0826ba6f3102bcca6bdc1892
jujemu <jujemu30@gmail.com>
2023-05-13 21:03:01 +0900

    yo

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,13 +1,15 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String helloController() {
+    public String helloController(Model model) {
+        model.addAttribute("username", "주제무");
         return "hello";
     }
 }
