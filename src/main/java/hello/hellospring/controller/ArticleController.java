498c222e4a286da8e6fd61b7a81f91fd5b8ed328
jujemu <jujemu30@gmail.com>
2023-05-26 17:30:12 +0900

    Spring boot - 10, Refactoring with lombok, slf4j

--- src/main/java/hello/hellospring/controller/ArticleController.java
+++ src/main/java/hello/hellospring/controller/ArticleController.java
@@ -3,12 +3,14 @@ package hello.hellospring.controller;
 import hello.hellospring.entity.Article;
 import hello.hellospring.dto.ArticleForm;
 import hello.hellospring.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class ArticleController {
 
     @Autowired
@@ -25,7 +27,7 @@ public class ArticleController {
 
         // give entity repository to put on db
         Article saved = articleRepository.save(article);
-        System.out.println(article.toString());
+        log.info(saved.toString());
 
         return "";
     }
--- src/main/java/hello/hellospring/dto/ArticleForm.java
+++ src/main/java/hello/hellospring/dto/ArticleForm.java
@@ -1,24 +1,15 @@
 package hello.hellospring.dto;
 
 import hello.hellospring.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
 
     private String title;
     private String content;
-
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
